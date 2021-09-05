ee5442093506f8f43babe41d812a7ad3431fc6d6
anvar89 <36979151+anvar89@users.noreply.github.com>
2021-09-05 22:29:40 +0300

    initial project
    Добавлен WeatherCast, ValuesHolder

---
+++ Lesson1_CrudController/Lesson1_CrudController/Controllers/CrudController/WeatherCast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1_CrudController.Controllers.CrudController
+{
+    public class WeatherCast
+    {
+        public int temoerature;
+        public DateTimeOffset dateTime;
+    }
+}
