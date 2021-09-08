e28b2f0d8861acffd53415e28f1c56cc233bb8ba
anvar89 <36979151+anvar89@users.noreply.github.com>
2021-09-08 23:03:58 +0300

    Добавлены контроллеры метрик

---
+++ Lesson2_RestApi/MetricsManager/Controllers/RamMetricsController.cs
@@ -0,0 +1,35 @@
+﻿using MetricsManager.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/ram")]
+    [ApiController]
+    public class RamMetricsController : ControllerBase
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] DateTimeOffset from, [FromRoute] DateTimeOffset to)
+        {
+            return Ok();
+        }
+
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAgent([FromRoute] int agentId, [FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime, [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] DateTimeOffset fromTime, [FromRoute] DateTimeOffset toTime, [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
+    }
+}
